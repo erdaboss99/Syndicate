0291da4cd7e27e0aed9c980e6be1f3bbb8fbde4e
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2024-01-08 00:00:16 +0100

    Implement Registration functionality

--- src/actions/registration.ts
+++ src/actions/registration.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import bcrypt from 'bcrypt';
 import * as z from 'zod';
+
+import { database } from '@/lib/prisma';
 
 import { RegistrationSchema } from '@/schemas';
 
@@ -10,5 +13,28 @@ export const registration = async (
 	const validatedData = RegistrationSchema.safeParse(values);
 
 	if (!validatedData.success) return { success: false, message: 'Invalid data!' };
-	else return { success: true, message: 'Account successfully created!' };
+
+	const { name, email, password } = validatedData.data;
+
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const existingUser = await database.user.findUnique({
+		where: {
+			email,
+		},
+	});
+
+	if (existingUser) return { success: false, message: 'This email is already taken!' };
+
+	await database.user.create({
+		data: {
+			name,
+			email,
+			password: hashedPassword,
+		},
+	});
+
+	// TODO: Send verification email token
+
+	return { success: true, message: 'Account successfully created!' };
 };
---
+++ src/data/user.ts
@@ -0,0 +1,19 @@
+import { database } from '@/lib/prisma';
+
+export const getUserByEmail = async (email: string) => {
+	try {
+		const user = await database.user.findUnique({ where: { email } });
+		return user;
+	} catch (error) {
+		return null;
+	}
+};
+
+export const getUserById = async (id: string) => {
+	try {
+		const user = await database.user.findUnique({ where: { id } });
+		return user;
+	} catch (error) {
+		return null;
+	}
+};

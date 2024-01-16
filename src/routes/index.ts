ad496c906e3d298057812b83e4c460349ec09bd6
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2024-01-16 23:39:28 +0100

    Implement middleware functionality

---
+++ src/routes/index.ts
@@ -0,0 +1,22 @@
+/**
+ * List of routes that are accessible to the public.
+ * These routes do not require any form of authentication.
+ */
+export const publicRoutes: string[] = ['/'];
+
+/**
+ * List of routes that are used for authentication.
+ * These routes will redirect logged in users to the home page.
+ */
+export const authRoutes: string[] = ['/auth/login', '/auth/registration'];
+
+/**
+ * This route is used for API authentication purposes.
+ * This route is accessible to anyone at any time.
+ */
+export const authAPIRoute: string = '/api/auth';
+
+/**
+ * This route is the default route to redirect to after logging in.
+ */
+export const DEFAULT_LOGIN_REDIRECT: string = '/';

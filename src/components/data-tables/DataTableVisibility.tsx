58a1ec0e2d26e10877fc38bfa0883b5bb1f48549
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2024-02-18 01:56:19 +0100

    Implement DataTableVisibility component

---
+++ src/components/data-tables/DataTableVisibility.tsx
@@ -0,0 +1,48 @@
+import { type Table } from '@tanstack/react-table';
+
+import { Button } from '@/components/ui/Button';
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuTrigger,
+} from '@/components/ui/DropdownMenu';
+import { LuEye } from 'react-icons/lu';
+
+type DataTableVisibilityProps<TData> = {
+	dataTable: Table<TData>;
+};
+
+const DataTableVisibility = <TData,>({ dataTable }: DataTableVisibilityProps<TData>) => {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant='outline'
+					size='sm'
+					className='ml-auto h-9 border'>
+					<LuEye className='mr-2 h-4 w-4' />
+					Columns visibility
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align='end'>
+				{dataTable
+					.getAllColumns()
+					.filter((column) => column.getCanHide())
+					.map((column) => {
+						return (
+							<DropdownMenuCheckboxItem
+								key={column.id}
+								className='capitalize'
+								checked={column.getIsVisible()}
+								onCheckedChange={(value) => column.toggleVisibility(!!value)}>
+								{column.id}
+							</DropdownMenuCheckboxItem>
+						);
+					})}
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
+
+export default DataTableVisibility;

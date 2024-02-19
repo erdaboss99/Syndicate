5d12a5a1950b79115371214942be65448c59a950
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2024-02-19 04:34:28 +0100

    Refactor Admin Dashboard page

--- src/app/(protected)/dashboard/@admin/page.tsx
+++ src/app/(protected)/dashboard/@admin/page.tsx
@@ -1,23 +1,44 @@
-import DashboardWrapper from '@/components/dashboard/DashboardWrapper';
-import DataTable from '@/components/data-tables/DataTable';
-import { UserColumns } from '@/components/data-tables/columns/UserColumns';
-import { userRoles } from '@/components/data-tables/filters';
+import Link from 'next/link';
+
 import { database } from '@/lib/database';
 
+import DashboardWrapper from '@/components/dashboard/DashboardWrapper';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card';
+import { LuUsers } from 'react-icons/lu';
+
 const AdminDashboardPage = async () => {
-	const users = await database.user.findMany();
+	const userCount = await database.user.aggregate({
+		_count: {
+			id: true,
+		},
+	});
+
+	const registeredInLastWeek = await database.user.aggregate({
+		_count: {
+			id: true,
+		},
+		where: {
+			createdAt: {
+				gte: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000),
+			},
+		},
+	});
 
 	return (
 		<DashboardWrapper headerTitle='Admin dashbord'>
-			<div className='mx-auto w-[95%]'>
-				<DataTable
-					columns={UserColumns}
-					data={users}
-					search='email'
-					filter={{ title: 'Role', columnKey: 'role', options: userRoles }}
-					visibility
-					pagination
-				/>
+			<div className='grid gap-4 p-4 md:grid-cols-2'>
+				<Link href='/users'>
+					<Card variant='tile'>
+						<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+							<CardTitle className='text-base md:text-lg'>Users</CardTitle>
+							<LuUsers className='h-6 w-6 text-muted-foreground' />
+						</CardHeader>
+						<CardContent>
+							<h4 className='text-xl font-bold'>{`${userCount._count.id} registered users`}</h4>
+							<p className='text-xs text-muted-foreground md:text-sm'>{`${registeredInLastWeek._count.id} new users in the last week`}</p>
+						</CardContent>
+					</Card>
+				</Link>
 			</div>
 		</DashboardWrapper>
 	);

2ac910ab9df881f69ef28996944031160cc174a1
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2024-02-19 03:24:51 +0100

    Refactor Navigation Records in UserButton

--- src/components/navbar/UserButton.tsx
+++ src/components/navbar/UserButton.tsx
@@ -2,6 +2,10 @@ import Link from 'next/link';
 
 import { getCurrentUser } from '@/lib/auth';
 
+import { type UserRole } from '@prisma/client';
+
+import { type IconType } from 'react-icons/lib';
+
 import LogoutButton from '@/components/auth/LogoutButton';
 import UserAvatar from '@/components/general/UserAvatar';
 import ThemeToggle from '@/components/navbar/ThemeToggle';
@@ -15,7 +19,34 @@ import {
 } from '@/components/ui/DropdownMenu';
 import { AiOutlineDashboard } from 'react-icons/ai';
 import { CgDarkMode } from 'react-icons/cg';
+import { LuUsers } from 'react-icons/lu';
 import { MdExitToApp, MdOutlineManageAccounts } from 'react-icons/md';
+
+const NavRecords: Omit<NavItemProps, 'currentRole'>[] = [
+	{ name: 'Account', href: '/account', roles: ['ADMIN', 'EMPLOYEE', 'USER'], Icon: MdOutlineManageAccounts },
+	{ name: 'Dashboard', href: '/dashboard', roles: ['ADMIN', 'EMPLOYEE', 'USER'], Icon: AiOutlineDashboard },
+	{ name: 'Users', href: '/users', roles: ['ADMIN'], Icon: LuUsers },
+];
+
+type NavItemProps = {
+	name: string;
+	href: string;
+	Icon: IconType;
+	roles: UserRole[];
+	currentRole: UserRole;
+};
+
+const Navitem = ({ name, href, Icon, roles, currentRole }: NavItemProps) => {
+	if (!roles.includes(currentRole)) return null;
+	return (
+		<Link href={href}>
+			<DropdownMenuItem>
+				<Icon className='mr-3 h-6 w-6' />
+				{name}
+			</DropdownMenuItem>
+		</Link>
+	);
+};
 
 const UserButton = async () => {
 	const user = await getCurrentUser();
@@ -32,26 +63,28 @@ const UserButton = async () => {
 			<DropdownMenuContent
 				className='w-40'
 				align='end'>
-				<Link href='/account'>
-					<DropdownMenuItem>
-						<MdOutlineManageAccounts className='mr-3 h-6 w-6' />
-						Account
-					</DropdownMenuItem>
-				</Link>
-				<Link href='/dashboard'>
-					<DropdownMenuItem>
-						<AiOutlineDashboard className='mr-3 h-6 w-6' />
-						Dashboard
-					</DropdownMenuItem>
-				</Link>
+				{NavRecords.map((item) => (
+					<Navitem
+						key={item.href}
+						name={item.name}
+						href={item.href}
+						roles={item.roles}
+						currentRole={user?.role!}
+						Icon={item.Icon}
+					/>
+				))}
+
 				<DropdownMenuSeparator />
+
 				<ThemeToggle>
 					<DropdownMenuItem>
 						<CgDarkMode className='mr-3 h-6 w-6' />
 						Switch theme
 					</DropdownMenuItem>
 				</ThemeToggle>
+
 				<DropdownMenuSeparator />
+
 				<LogoutButton>
 					<DropdownMenuItem>
 						<MdExitToApp className='mr-3 h-6 w-6' />

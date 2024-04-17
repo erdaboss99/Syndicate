c65600b0b9428bb9757fc91ddd75cf9f0fafcb3f
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2024-04-17 03:13:31 +0200

    Adjust Playwright config, Create Automatic Regression Test Github Action workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,27 +7,58 @@ export default defineConfig({
 	testDir: './tests',
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 2 : 0,
-	workers: process.env.CI ? 1 : undefined,
+	retries: process.env.CI ? 1 : 1,
+	workers: process.env.CI ? 1 : 1,
 	reporter: 'html',
 	use: {
-		trace: 'on-first-retry',
+		trace: 'retain-on-failure',
+		video: 'retain-on-failure',
+		baseURL: process.env.BASE_URL,
+		navigationTimeout: 20 * 1000,
+	},
+
+	expect: {
+		timeout: 20 * 1000,
 	},
 
 	projects: [
+		{ name: 'Setup', testMatch: /.*\.setup\.ts/, teardown: 'Teardown' },
 		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] },
+			name: 'Chromium',
+			use: {
+				...devices['Desktop Chrome'],
+			},
+			dependencies: ['Setup'],
+		},
+		{
+			name: 'Firefox',
+			use: {
+				...devices['Desktop Firefox'],
+			},
+			dependencies: ['Setup'],
+		},
+		{
+			name: 'Safari',
+			use: {
+				...devices['Desktop Safari'],
+			},
+			dependencies: ['Setup'],
+		},
+		{
+			name: 'iPhone 13 Pro Max',
+			use: {
+				...devices['iPhone 13 Pro Max'],
+			},
+			dependencies: ['Setup'],
+		},
+		{
+			name: 'Pixel 7',
+			use: {
+				...devices['Pixel 7'],
+			},
+			dependencies: ['Setup'],
 		},
 
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] },
-		},
-
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] },
-		},
+		{ name: 'Teardown', testMatch: /.*\.teardown\.ts/ },
 	],
 });
